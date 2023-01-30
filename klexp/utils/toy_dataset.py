cde1fb12af818386c7ce869dc337bb1505ee3389
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2023-01-30 17:21:31 -0600

    plot dataset

--- klexp/utils/toy_dataset.py
+++ klexp/utils/toy_dataset.py
@@ -1,7 +1,23 @@
 import numpy as np
 
 # Creat toy dataset of pairs of coordinates and function values
-def toy_dataset(n=200, s=100, d=1, x_range=(-10, 10)):
+def toy_dataset(n=200, s=100, d=1, x_range=(-10, 10), same_s=True):
+    """Creat quadratic toy dataset of pairs of coordinates and function values.
+
+    This toy dataset is obtained from: https://arxiv.org/pdf/2209.14125.pdf.
+
+    Args:
+        n: Number of data points.
+        s: Maximum number of points at which functions is evaluated.
+        d: Dimension of the input space.
+        x_range: Range of the input space.
+        same_s: If True, all functions are evaluated at the same s points.
+
+    Returns:
+        A list of n data points. Each data point is a tuple of two arrays with
+        the first array being the coordinates and the second array being the
+        function values.
+    """
     data = []
     for i in range(n):
         a = np.random.uniform(-1, 1, size=(d,))
